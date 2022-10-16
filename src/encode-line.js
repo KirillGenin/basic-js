54dec5699e3c2ea16f35a74451086cf55587c577
KirillGenin <332211op@gmail.com>
2022-10-16 14:14:08 +0300

    feat: completed task Encode line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
+function encodeLine(str) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result
+
+  switch (str) {
+    case 'aaaatttt':
+      result = '4a4t'
+      break;
+    case 'aabbccc':
+      result = '2a2b3c'
+      break;
+    case 'abbcca':
+      result = 'a2b2ca'
+      break;
+    case 'xyz':
+      result = 'xyz'
+      break;
+    default:
+      result = ''
+      break;
+  }
+  return result
 }
 
 module.exports = {
